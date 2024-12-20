1a9e3655b40837ab3813cf5fd1c50123be65496b
Facundo Marroquín <a01383541@tec.mx>
2024-12-20 13:04:55 -0600

    delete code

--- swedishhubproj/src/app/page.tsx
+++ swedishhubproj/src/app/page.tsx
@@ -15,10 +15,6 @@ export default function Home() {
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started wiith the swedishub{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
           </li>
           <li>Se vienen cositas</li>
         </ol>
