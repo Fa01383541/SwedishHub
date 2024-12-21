457bf813a7cd06f5fd7c65203df968ffb64bf5c4
Facundo Marroquín <a01383541@tec.mx>
2024-12-20 18:31:55 -0600

    de Fa

--- swedishhubproj/src/app/page.tsx
+++ swedishhubproj/src/app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-white text-4xl font-bold">The Swedish Hub</h1>
+        <h1 className="text-black dark:text-white text-4xl font-bold">The Swedish Hub</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started with the swedishub{" "}
+            Get started with the swedishub de Fa{" "}
           </li>
           <li>Se vienen cositas</li>
         </ol>
